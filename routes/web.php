5aedac317c60b00845716d31810d9cdd56aa8748
Dmitrii <korsunov2006@list.ru>
2022-12-08 19:30:03 +0600

    layout added

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//navigation
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', function () {
     return view('blog.home');
 });
