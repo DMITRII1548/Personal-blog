dfa9c27772916dfc17f51a47902e284552738848
Dmitrii <korsunov2006@list.ru>
2022-12-12 19:10:33 +0600

    about page added

--- app/Http/Controllers/Nav/AboutController.php
+++ app/Http/Controllers/Nav/AboutController.php
@@ -10,7 +10,7 @@ class AboutController extends Controller
     
     public function __invoke()
     {
-        //code
+        return view('blog.about');
     }
  
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -5,5 +5,6 @@ import '../assets/css/media/home.css'
 import '../assets/css/reset.css'
 import '../assets/css/pagination.css'
 import '../assets/css/article/show.css'
+import '../assets/css/about.css'
 
 import '../assets/js/home'
