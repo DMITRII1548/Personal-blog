5ad3f70afcc462944bd654fa6eb32577a58362fb
Dmitrii <korsunov2006@list.ru>
2022-12-23 19:54:54 +0600

    DestroyControlles modified

--- app/Http/Controllers/Article/DestroyController.php
+++ app/Http/Controllers/Article/DestroyController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Article;
 use App\Http\Controllers\Controller;
 use App\Models\Article;
 use App\Models\ArticleComment;
+use Exception;
 use Illuminate\Http\Request;
 
 class DestroyController extends Controller
@@ -12,8 +13,15 @@ class DestroyController extends Controller
 
     public function __invoke(Article $article)
     {
-        ArticleComment::where('article_id', $article->id)->forceDelete();
-        $article->delete();
+        try {
+            ArticleComment::where('article_id', $article->id)->forceDelete();
+            $article->delete();
+
+            return redirect()->route('nav.home');
+        } catch (Exception $e) {
+            return redirect()->route('nav.home');
+        }
+
     }
 
 }
